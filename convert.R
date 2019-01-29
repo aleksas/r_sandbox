1d355468a599d52f70524623bea0de976b9e5088
Aleksas Pielikis <ant.kampo@gmail.com>
2019-01-29 22:09:16 +0000

    Initial commit

---
+++ convert.R
@@ -0,0 +1,14 @@
+library(tools)
+
+rmd2jupyter <- function( filename, path=getwd() ) 
+{
+  path_in <- paste( path, "/", filename, " ", sep="" )
+  path_out <- paste(path, "/", file_path_sans_ext(filename), ".ipynb", sep="")
+  full_shell <- paste("notedown ", path_in, " --rmagic --run > ", path_out, sep="")
+  system(full_shell)
+}
+
+# download the example.Rmd file from this repository
+# setwd(...) to where the example.Rmd file is located
+
+rmd2jupyter( "sample_notebook.Rmd" )
